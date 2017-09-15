fbd7b9c6bc7027def341d3b52ffd6912d44b986d
Thomas Delhoménie <thomas.delhomenie@gmail.com>
2017-09-15 12:24:11 +0200

    SOC-5797 : use Social JPA data storage in tests instead of JCR one (#20)

--- integration/src/test/java/org/exoplatform/forum/ext/faq/FAQServiceBaseTestCase.java
+++ integration/src/test/java/org/exoplatform/forum/ext/faq/FAQServiceBaseTestCase.java
@@ -40,10 +40,10 @@ import org.exoplatform.services.security.Identity;
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.identity-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.core.test.configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.search.test.configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.component.core.test.configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.test.jcr-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.test.portal-configuration.xml"),
-  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.core.test.configuration.xml"),  
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/rest/exo.faq.component.service.test.configuration.xml")
 })
 public abstract class FAQServiceBaseTestCase extends BaseExoTestCase {
